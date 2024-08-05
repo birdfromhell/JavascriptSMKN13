e5726e62389d2a8f4b301a7f69aba6393bfe7ccd
birdfromhell <95958484+birdfromhell@users.noreply.github.com>
2024-08-05 01:43:30 +0000

    feat: Add looping examples in KELAS-12 folder

---
+++ KELAS-12/looping.js
@@ -0,0 +1,20 @@
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+let myArray = ['A', 'B', 'C', 'D', 'E'];
+for (let array of myArray) {
+  console.log(array);
+}
+
+let angka = 5
+while (angka <= 10) {
+  console.log(angka);
+  angka++
+}
+
+let jawab = 'ya'
+do{
+  console.log("Halo Dek")
+  jawab = "tidak"
+}while(jawab.toLowerCase() === "ya")
